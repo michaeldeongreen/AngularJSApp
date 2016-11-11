72c20e9b7d1768d94cce59e344047b48b9267a4d
michaeldeongreen <michael.green@architectinginnovation.com>
2016-11-11 10:53:13 -0600

    *Added new library for model and installed structuremap mvc 4 for web api and regular mvc

---
+++ AngularJSApp.Model/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularJSApp.Model
+{
+    public class Class1
+    {
+    }
+}
